951b1281a6385fc6418b05d7293f266587d928fa
shreyas <shreyasseshadri@gmail.com>
2019-11-20 17:04:19 +0530

    Modificatiobs

--- programs/memory.py
+++ programs/memory.py
@@ -1,2 +1,2 @@
-a = ["1", "2"]  # TOK str
-b = [i for i in range(3)]  # TOK num
+a = ["1", "2"]  #TOK str
+b = [i for i in range(3)]  #TOK num
--- utilization.sh
+++ utilization.sh
@@ -1,6 +1,10 @@
+if [ -f "util.txt" ] ; then
+    echo "Removing previous utilization output"
+    rm "util.txt"
+fi
 top -b -n3  | grep -f patterns.txt > util.txt
 echo "CPU charecteristics taken"
 timeout 3s bwm-ng -o csv > nw.txt
-grep "wlp2s0" nw.txt >> util.txt
+grep -f patterns.txt nw.txt >> util.txt
 echo "Network charecteristics taken"
 rm nw.txt
