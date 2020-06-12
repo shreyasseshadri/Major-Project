1c9359941368cbaaf9cd8eda097a7423b3f02e0a
shreyas <shreyasseshadri@gmail.com>
2020-06-12 13:32:44 +0530

    Formatting

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import threading
 from flask import Flask, request
 import util_extractor as ue
 import tempfile
@@ -6,28 +7,28 @@ import vm_allocator as allocator
 import analyzer
 app = Flask(__name__)
 total_VMs = 0
-import threading
+
 
 @app.route('/util', methods=['POST'])
 def handle_util_submit():
-	if request.method == "POST":
-		file_data = list(request.form.to_dict().keys())[0]
+    if request.method == "POST":
+        file_data = list(request.form.to_dict().keys())[0]
 
-	with open('temp.txt', 'w') as f:
-		f.write(file_data)
+    with open('temp.txt', 'w') as f:
+        f.write(file_data)
 
-	allocator.insert_new_vm('temp.txt')
-	os.remove('temp.txt')
-	return 'Done'
+    allocator.insert_new_vm('temp.txt')
+    os.remove('temp.txt')
+    return 'Done'
 
 
 if __name__ == "__main__":
-    t1 = threading.Thread(target=app.run,kwargs={'host':'0.0.0.0'})
+	t1 = threading.Thread(target=app.run, kwargs={'host': '0.0.0.0'})
 	t1.start()
 	try:
 		while True:
 			if(len(allocator.sorted_usage_list[0]) == 3):
-				print('\n','='*90,'\n')
+				print('\n', '='*90, '\n')
 				print('Sorted List of available resources')
 				for i, l in enumerate(allocator.sorted_usage_list):
 					print(allocator.index2name(i)+" : "+str(l))
@@ -43,7 +44,7 @@ if __name__ == "__main__":
 
 				# print(allocator.vm_queue)
 				allocator.print_queue()
-				print('\n','='*90,'\n')
+				print('\n', '='*90, '\n')
 				allocator.clear_queue()
 				print('Testing Dependent task2Vm by example 1')
 
@@ -57,7 +58,7 @@ if __name__ == "__main__":
 				task_scores = {
 					't1': (0.2, 0.3, 0.5, None, None, None),
 					't2': (0.3, 0.2, 0.4, None, None, None),
-					't3': (0.5, 0.49, 0.01, None, None, None)
+						't3': (0.5, 0.49, 0.01, None, None, None)
 				}
 
 				# print(task_scores)
@@ -66,10 +67,11 @@ if __name__ == "__main__":
 				# print(allocator.vm_queue)
 				allocator.print_queue()
 
-				print('\n','='*90,'\n')
+				print('\n', '='*90, '\n')
 				allocator.clear_queue()
 
-				print('Testing Dependent task2Vm by memory.py, compute.py, network.py')
+				print(
+					'Testing Dependent task2Vm by memory.py, compute.py, network.py')
 
 				file_names = ['memory.py', 'compute.py', 'network.py']
 				task_scores = {f: analyzer.analyze(
@@ -77,7 +79,8 @@ if __name__ == "__main__":
 
 				# print(task_scores)
 				allocator.print_task_scores(task_scores)
-				sorted_task_names = allocator.create_sorted_task_names(task_scores)
+				sorted_task_names = allocator.create_sorted_task_names(
+					task_scores)
 
 				allocator.dependent_Task2Vm(task_scores, sorted_task_names)
 
@@ -86,22 +89,23 @@ if __name__ == "__main__":
 
 				allocator.clear_queue()
 
-				print('\n','='*90,'\n')
+				print('\n', '='*90, '\n')
 				# Another example
 
 				print('Testing Dependent Task2Vm by example 2')
 				task_scores = {
 					't1': (0.2, 0.3, 0.5, None, None, None),
 					't2': (0.3, 0.49, 0.01, None, None, None),
-					't3': (0.5, 0.2, 0.3, None, None, None)
+						't3': (0.5, 0.2, 0.3, None, None, None)
 				}
 				# print(task_scores)
 				allocator.print_task_scores(task_scores)
-				allocator.dependent_Task2Vm(task_scores, allocator.create_sorted_task_names(task_scores))
+				allocator.dependent_Task2Vm(
+					task_scores, allocator.create_sorted_task_names(task_scores))
 				# print(allocator.vm_queue)
 				allocator.print_queue()
 
-				print('\n','='*90,'\n')
+				print('\n', '='*90, '\n')
 				t1.join()
 				exit()
 				break
